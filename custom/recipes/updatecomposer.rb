d99ddf06b981a793e371e0eb7b05317ba820ed9c
Carlos Afonso <cafonso@e-valua.es>
2015-03-12 15:57:08 +0100

    .

--- custom/recipes/updatecomposer.rb
+++ custom/recipes/updatecomposer.rb
@@ -1,7 +1,7 @@
 include_recipe "composer"
 
 node[:deploy].each do |application, deploy|
-	composer_composer_package "#{deploy[:deploy_to]}" do
+	composer_package "#{deploy[:deploy_to]}" do
 		Chef::Log.debug("Will do composer update on #{deploy[:deploy_to]}")
 		action [:update]
 	end

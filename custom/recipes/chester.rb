59c5c72b8331798de6bdf3a65b2c36ffe119b713
Carlos Afonso <cafonso@e-valua.es>
2015-03-13 15:40:49 +0100

    .

---
+++ custom/recipes/chester.rb
@@ -0,0 +1,17 @@
+Chef::Log.info("Test Chef recipe!!")
+Chef::Log.info("#{node[:deploy]['opsworks'][:scm][:ssh_key]}")
+
+include_recipe "composer"
+
+execute "bazinga" do
+        cwd "#{node[:deploy]['opsworks'][:deploy_to]}/current"
+        command "echo '#{node[:deploy]['opsworks'][:scm][:ssh_key]}' > ~/.ssh/deploykey"
+        command "chmod 400 ~/.ssh/deploykey"
+        command "eval `ssh-agent -s`"
+        command "ssh-add ~/.ssh/deploykey"
+        command "composer update --no-interaction --no-ansi"
+end
+
+#composer_package "/srv/www/opsworks/current" do
+#       action :install
+#end
